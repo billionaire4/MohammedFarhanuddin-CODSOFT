20977426d2270ef23855484434febf58871bfdb8
MohammedFarhanuddin <86943970+billionaire4@users.noreply.github.com>
2024-06-30 20:43:17 +0530

    Add files via upload

---
+++ codsoft/Level-1_Task-3(Calculator)/calculator.js
@@ -0,0 +1,22 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function deleteLast() {
+    var current = document.getElementById("result").value;
+    document.getElementById("result").value = current.slice(0, -1);
+}
+
+function appendCharacter(character) {
+    document.getElementById("result").value += character;
+}
+
+function calculateResult() {
+    var result = document.getElementById("result").value;
+    try {
+        document.getElementById("result").value = eval(result);
+    } catch (e) {
+        alert("Invalid input");
+        clearScreen();
+    }
+}
